d35e7ee2abfcaf0bc7948d681003fb21935856e4
Mohammed Wasim <mohammed.wasimuddin@rewe-group.com>
2026-08-30 22:02:30 +0200

    Add backend service

---
+++ task-manager-service/src/main/java/com/paxier/task_manager_service/TaskManagerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.paxier.task_manager_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerServiceApplication {
+
+	 static void main(String[] args) {
+		SpringApplication.run(TaskManagerServiceApplication.class, args);
+	}
+
+}
