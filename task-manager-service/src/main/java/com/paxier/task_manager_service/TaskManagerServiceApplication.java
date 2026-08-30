2f83477a2dd398fb014d63bd8923e6de072eebc9
Mohammed Wasim <mohammed.wasimuddin@rewe-group.com>
2026-08-30 22:04:24 +0200

    Refactor Application entry point

--- task-manager-service/src/main/java/com/paxier/task_manager_service/TaskManagerServiceApplication.java
+++ task-manager-service/src/main/java/com/paxier/task_manager_service/TaskManagerServiceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TaskManagerServiceApplication {
 
-	 static void main(String[] args) {
-		SpringApplication.run(TaskManagerServiceApplication.class, args);
-	}
+  void main() {
+    SpringApplication.run(TaskManagerServiceApplication.class);
+  }
 
 }
