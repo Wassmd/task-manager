a46778632d4c53cdb3105dbc5f2076477b36443f
Mohammed Wasim <mohammed.wasimuddin@rewe-group.com>
2026-09-06 19:42:42 +0200

    Add Repository test and add flyway

---
+++ task-manager-service/src/test/java/com/paxier/task_manager_service/repository/TaskRepositoryTest.java
@@ -0,0 +1,30 @@
+package com.paxier.task_manager_service.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import com.paxier.task_manager_service.model.TaskEntity;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+
+@DataJpaTest
+class TaskRepositoryTest {
+
+  @Autowired
+  private TaskRepository taskRepository;
+
+  @Test
+  void testTask_isBeingSaved() {
+    TaskEntity task = TaskEntity.builder()
+        .title("Test Task")
+        .description("This is a test task")
+        .status(com.paxier.task_manager_service.api.model.TaskStatus.OPEN)
+        .build();
+
+    TaskEntity savedEntity = taskRepository.save(task);
+
+    assertThat(savedEntity).isNotNull();
+    assertThat(savedEntity.getId()).isNotNull();
+    assertThat(savedEntity.getTitle()).isEqualTo("Test Task");
+
+  }
+}
